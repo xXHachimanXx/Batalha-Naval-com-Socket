dc2f42ead92decd67b74a22e5588d9d2c1266a7b
xXHachimanXx <globaldep90@gmail.com>
2020-10-28 18:47:27 -0300

    Criando métodos de estabelecimanto e validação de conexão.

--- src/server.py
+++ src/server.py
@@ -1,6 +1,10 @@
+import socket
 from random import randint
 
 from constants import *
+
+serversocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+serverBoard = []
 
 def createBoard():
     matrix = []
@@ -50,3 +54,101 @@ def createBoard():
                     fits = True
                     
     return matrix
+
+def initServer(port):
+    """Método para inicializar o servidor e carrgar dados da database.        
+
+        Parameters
+        ----------
+        port : int
+            A porta a ser escutada pelo servidor.
+    """
+    global serversocket
+    global serverBoard
+    
+    # pegar nome da maquina local
+    host = socket.gethostname()
+    
+    # conectar e setar tamanho do backlog
+    serversocket.bind((host, port))
+    
+    # setar tabuleiro do servidor
+    serverBoard = createBoard()
+    
+    print("Servidor pronto para jogar!\n")
+    
+def checkInput(msg):
+    """Função para checar entrada do client.
+
+        Parameters
+        ----------
+        msg : str
+            Mensagem do client.
+            
+        Returns
+        -------
+        boolean
+           Validez da mensagem
+    """
+    return (
+        len(msg) == 3 and
+        (msg[0] == 'J')
+    )
+
+def consoleWarning(msg, success, address):
+    """Método para mostrar a situação da operação.
+
+        Parameters
+        ----------
+        msg : str
+            Mensagem do client.
+        success : boolean
+            Flag de sucesso/fracasso da operação.
+        client : socket
+            Objeto que representa o client da operação.
+    """
+    successFlag = "Sucesso"
+    
+    if not success:        
+        successFlag = "Fracasso"
+        
+    print(
+        "Client: " + address[0] + ":" + str(address[1]) +
+        "\nMensagem: " + str(msg) +
+        "\nStatus: " + successFlag + "\n"        
+    )  
+
+def main():
+    print("Bem-vindo à Batalha Naval com Socket!")
+    port = int(input("Digite a porta a ser escutada pelo servidor para inicializá-lo: "))
+    
+    initServer(port)
+
+    while True:
+        global serverBoard
+        global serversocket
+        success = False
+        
+        msg, client = serversocket.recvfrom(1024)        
+        msg = msg.decode('utf-8').split(' ')
+        
+        if(checkInput(msg)):
+            
+            # cadastro
+            if msg[0] == 'J':
+                print("Mensagem recebida!")
+                success = True
+                consoleWarning(msg, success, client)
+                # if shotStatus ? 
+                serversocket.sendto("Status tiro".encode('utf-8'), client)
+        else:            
+            consoleWarning(msg, success, client)
+            serversocket.sendto(
+                "Server - Erro: Entrada de dados inválida!".encode("utf-8"),
+                client
+            )
+
+    # Fechar conexao
+    serversocket.close()
+
+main()
