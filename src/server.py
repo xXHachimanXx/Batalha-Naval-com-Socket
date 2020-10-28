f2b95613a09ada715b5b55d78dfd40abbfc1000e
xXHachimanXx <globaldep90@gmail.com>
2020-10-28 17:36:47 -0300

    Criando método para geração de tabuleiros aleatórios.

--- src/server.py
+++ src/server.py
@@ -10,18 +10,43 @@ def createBoard():
         for y in range(SIZE_BOARD):
             matrix[x][y] = -1
     
-    # Iterar para encaixar todos os navios
-    for shipSize in TYPES_OF_SHIPS.values():
+    # Iterar para encaixar todos os navios    
+    for ship, shipSize in TYPES_OF_SHIPS:
         fits = False
         
         # Enquanto nao couber o navio
         while not fits:
             
-            insertPosition = randint(0, 9)
+            insertPositionX = randint(0, 9)
+            insertPositionY = randint(0, 9)
             
-            if insertPosition + shipSize <= SIZE_BOARD:                
-                for target_list in expression_list:
-                    pass
-            
-    
-    return None
+            # Testar se nao ultrapassa o tabuleiro
+            if insertPositionX + shipSize <= SIZE_BOARD and \
+               insertPositionY + shipSize <= SIZE_BOARD:
+                   
+                # Testar se o espaço nao esta ocupado
+                columnOccupied = False
+                for x in range(shipSize):
+                    if not matrix[x+insertPositionX][insertPositionY] == -1:
+                        columnOccupied = True
+                        break
+                # Testar se o espaço nao esta ocupado
+                lineOccupied = False
+                for y in range(shipSize):
+                    if not matrix[insertPositionX][y+insertPositionY] == -1:
+                        lineOccupied = True
+                        break
+                
+                # Se couber, insira na matriz
+                if not lineOccupied:
+                    for x in range(insertPositionX):
+                        for y in range(insertPositionY):
+                            matrix[x][y] = ship
+                    fits = True
+                elif not columnOccupied:
+                    for x in range(insertPositionX):
+                        for y in range(insertPositionY):
+                            matrix[x][y] = ship
+                    fits = True
+                                                                                     
+    return matrix
