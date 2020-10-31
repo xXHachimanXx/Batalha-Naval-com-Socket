c7c2872eee28f8d76e5284a72848dc62b642bca2
xXHachimanXx <globaldep90@gmail.com>
2020-10-31 11:04:07 -0300

    Adicionando validação e resposta ao tiro do cliente.

--- src/client.py
+++ src/client.py
@@ -1,92 +1,128 @@
 import ipaddress
-import socket 
+import socket
 
 # criar um objeto socket
 udpConnection = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+clientBoard = None
+clientShips = {"1": 5, "2": 4, "3": 3, "4": 2}
+clientHitsCounter = 5 + 4 + 3 + 2
 
 # Método para testar conexão
-def testIp(destiny):    
+def testIp(destiny):
     ip = destiny[0]
     # testar ip
-    
-    ip = ipaddress.ip_address(ip)                
-    
-    return (ip != None)
-    
+
+    ip = ipaddress.ip_address(ip)
+
+    return ip != None
+
+
 # Ler dados do usuário
-def readClientData():
-    
+def readConnectionData():
+
     while True:
-        destiny = input("Informe o ip e a porta da seguinte forma: <ip> <porta>\n").split(' ')        
-        
+        # destiny = input(
+        #     "Informe o ip e a porta da seguinte forma: <ip> <porta>\n"
+        # ).split(" ")
+        # DEBUG
+        destiny = ["localhost", 3333]
+
         if destiny[0] == "localhost":
             hostname = socket.gethostname()
             destiny[0] = socket.gethostbyname(hostname)
 
         if len(destiny) == 2 and testIp(destiny):
             break
-        else:      
-            print(destiny)      
+        else:
+            print(destiny)
             print("Ip ou porta inválida! Tente de novo: ")
 
     return destiny
 
+
 def youWinOrLose(msg):
     resp = False
-    
+
     if msg == "Ganhou":
         resp = True
         print("Parabéns vocẽ ganhou :D")
-        
+
     elif msg == "Perdeu":
         resp = True
         print("Que pena você perdeu (T_T)")
-    
+
     return resp
+
+
+def countShot(msg):
+    global clientBoard
+    msg = msg.split()
+
 
 def main():
     global udpConnection
-    
-    destiny = readClientData()
-    
+
+    # Receber dados para a conexao
+    destiny = readConnectionData()
     host = destiny[0]
     port = int(destiny[1])
-    
+
+    # Conectar ao servidor
     udpConnection.connect((host, port))
-    
-    flagToExit = False        
-    
+
+    # Flag caso o usuário queira sair do programa
+    flagToExit = False
+
+    # Status iniciais
+    statusFire = "Errou"
+    typeShipHitted = "-1"
+
+    # Fluxo do jogo
     while not flagToExit:
-        
-        print("\nEntre com um código: " + \
-              "\nJogar - J <numero> <letra>" + \
-              "\nSair da aplicação - E")
-        
+
+        print(
+            "\nEntre com um código: "
+            + "\nJogar - J <letra> <numero>"
+            + "\nSair da aplicação - E"
+        )
+
         try:
             msg = input()
+            msg = msg.split(" ")
+
+            # msg -> J <Errou|Acertou> <tipo-navio> <letra> <numero>
+            msg = (
+                msg[0]
+                + " "
+                + statusFire
+                + " "
+                + typeShipHitted
+                + " "
+                + " ".join(msg[1:])
+            )
         except EOFError:
             print(msg)
-        
+
         # testar se e' uma flag de saida
-        if msg == "E":
+        if msg[0] == "E":
             flagToExit = True
-            print('Obrigado e até logo :)')
+            print("Obrigado e até logo :)")
             continue
-        
-        elif msg == "J":
-            msg = msg.encode('UTF-8')
+
+        elif msg[0] == "J":
+            msg = msg.encode("UTF-8")
             udpConnection.sendto(msg, (host, port))
-        
-            # udpConnection.settimeout(5.0)
+
+            # Recuperar mensagem do servidor
             msg, client = udpConnection.recvfrom(1024)
-            
+
+            countShot(msg)
+            print(msg.decode("utf-8"))
             # Se ganhou ou perdeu, saia do loop
-            if youWinOrLose(msg):
-                flagToExit = True                
+            if youWinOrLose(msg[0]):
+                flagToExit = True
         else:
             print("\nCódigo inválido")
-        
-          
 
 
 main()
--- src/server.py
+++ src/server.py
@@ -5,27 +5,32 @@ from constants import *
 
 serversocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 serverBoard = None
+serverHitsCounter = 5 + 4 + 3 + 2
+
+serverShips = {"1": 5, "2": 4, "3": 3, "4": 2}
 
 def createBoard():
     matrix = None
-    
+
     # Inicializa matriz com -1
-    matrix = [[-1 for x in range(SIZE_BOARD)] for y in range(SIZE_BOARD)] 
-    
-    # Iterar para encaixar todos os navios    
+    matrix = [[-1 for x in range(SIZE_BOARD)] for y in range(SIZE_BOARD)]
+
+    # Iterar para encaixar todos os navios
     for ship in TYPES_OF_SHIPS:
         fits = False
-                
+
         # Enquanto nao couber o navio
         while not fits:
-            
+
             insertPositionX = randint(0, 9)
             insertPositionY = randint(0, 9)
-            
+
             # Testar se nao ultrapassa o tabuleiro
-            if insertPositionX + TYPES_OF_SHIPS[ship] <= SIZE_BOARD and \
-               insertPositionY + TYPES_OF_SHIPS[ship] <= SIZE_BOARD:
-                   
+            if (
+                insertPositionX + TYPES_OF_SHIPS[ship] <= SIZE_BOARD
+                and insertPositionY + TYPES_OF_SHIPS[ship] <= SIZE_BOARD
+            ):
+
                 # Testar se o espaço nao esta ocupado
                 columnOccupied = False
                 for x in range(TYPES_OF_SHIPS[ship]):
@@ -38,7 +43,7 @@ def createBoard():
                     if not matrix[insertPositionX][y + insertPositionY] == -1:
                         lineOccupied = True
                         break
-                
+
                 # Se couber, insira na matriz
                 if not lineOccupied:
                     for y in range(TYPES_OF_SHIPS[ship]):
@@ -48,104 +53,179 @@ def createBoard():
                     for x in range(TYPES_OF_SHIPS[ship]):
                         matrix[x][insertPositionY] = ship
                     fits = True
-                    
+
     return matrix
 
-def initServer(port):
-    """Método para inicializar o servidor e carrgar dados da database.        
 
-        Parameters
-        ----------
-        port : int
-            A porta a ser escutada pelo servidor.
+def initServer(port):
+    """Método para inicializar o servidor e carrgar dados da database.
+
+    Parameters
+    ----------
+    port : int
+        A porta a ser escutada pelo servidor.
     """
     global serversocket
     global serverBoard
-    
+
     # pegar nome da maquina local
     host = socket.gethostname()
-    
+
     # conectar e setar tamanho do backlog
     serversocket.bind((host, port))
-    
+
     # setar tabuleiro do servidor
     serverBoard = createBoard()
-    
+
     print("Servidor pronto para jogar!\n")
-    
+
+
 def checkInput(msg):
     """Função para checar entrada do client.
 
-        Parameters
-        ----------
-        msg : str
-            Mensagem do client.
-            
-        Returns
-        -------
-        boolean
-           Validez da mensagem
+    Parameters
+    ----------
+    msg : str
+        Mensagem do client.
+
+    Returns
+    -------
+    boolean
+       Validez da mensagem
     """
-    return (
-        len(msg) == 3 and
-        (msg[0] == 'J')
-    )
+    return len(msg) == 5 and (msg[0] == "J")
+
 
 def consoleWarning(msg, success, address):
     """Método para mostrar a situação da operação.
 
-        Parameters
-        ----------
-        msg : str
-            Mensagem do client.
-        success : boolean
-            Flag de sucesso/fracasso da operação.
-        client : socket
-            Objeto que representa o client da operação.
+    Parameters
+    ----------
+    msg : str
+        Mensagem do client.
+    success : boolean
+        Flag de sucesso/fracasso da operação.
+    client : socket
+        Objeto que representa o client da operação.
     """
     successFlag = "Sucesso"
-    
-    if not success:        
+
+    if not success:
         successFlag = "Fracasso"
-        
+
     print(
-        "Client: " + address[0] + ":" + str(address[1]) +
-        "\nJogada: " + str(msg) +
-        "\nStatus: " + successFlag + "\n"        
-    )  
+        "Client: "
+        + address[0]
+        + ":"
+        + str(address[1])
+        + "\nJogada: "
+        + str(msg)
+        + "\nStatus: "
+        + successFlag
+        + "\n"
+    )
+
+
+def getBoardLine(boardLetter):
+
+    boardLine = ord(boardLetter.lower()) - 97
+
+    # Testar se a posição não é válida
+    if not (boardLine >= 0 and boardLine <= 9):
+        boardLine = -1
+
+    return boardLine
+
+
+def countShot(line, column):
+    global serverHitsCounter
+    
+    # tentar pegar posição de algum navio
+    position = serverBoard[line][column]
+    
+    if position == -1:
+        response = "Errou -1"
+    else:
+        response = "Acertou " + str(serverBoard[line][column])
+        serverBoard[line][column] = -1
+
+    return response
+
+
+def validateShot(msg):
+
+    # Pegar linha e coluna do tabuleiro
+    line = msg[3]  # linha
+    column = msg[4]  # coluna
+
+    response = ""
+
+    # Testar se linha é uma letra e se a coluna é um numero
+    if line.isalpha() and column.isnumeric():
+
+        # transformar letra em um índice
+        line = getBoardLine(line)
+        column = int(column)
+
+        # Validar indices linha e coluna
+        if line >= 0 and (column >= 0 and column <= 9):
+            response += countShot(line, column)
+        else:
+            response += "Errou -1"
+    else:
+        response += "Errou -1"
+
+    return response
+
+def shot():
+    line = randint(0, 9)
+    column = randint(0, 9)
+    
+    return chr(line + 97).upper() + " " + str(column)
+
 
 def main():
     print("Bem-vindo à Batalha Naval com Socket!")
-    port = int(input("Digite a porta a ser escutada pelo servidor para inicializá-lo: "))
-    
+    # port = int(
+    #     input("Digite a porta a ser escutada pelo servidor para inicializá-lo: ")
+    # )
+
+    # DEBUG
+    port = 3333
+
     initServer(port)
+    
+    global serverBoard
+    print(serverBoard)
 
     while True:
-        global serverBoard
         global serversocket
+        global serverHitsCounter
         success = False
-        
-        msg, client = serversocket.recvfrom(1024)        
-        msg = msg.decode('utf-8').split(' ')
-        
-        if(checkInput(msg)):
-            
+
+        msg, client = serversocket.recvfrom(1024)
+        msg = msg.decode("utf-8").split(" ")
+
+        if checkInput(msg):
+
             # cadastro
-            if msg[0] == 'J':
+            if msg[0] == "J":
                 print("Mensagem recebida!")
                 success = True
                 consoleWarning(msg, success, client)
-                #
-                serversocket.sendto("Status tiro".encode('utf-8'), client)
-        else:            
+                response = validateShot(msg)
+                response += " " + shot()
+                
+                serversocket.sendto(response.encode("utf-8"), client)
+        else:
             consoleWarning(msg, success, client)
             serversocket.sendto(
-                "Server - Erro: Entrada de dados inválida!".encode("utf-8"),
-                client
+                "Server - Erro: Entrada de dados inválida!".encode("utf-8"), client
             )
 
     # Fechar conexao
     serversocket.close()
 
-#createBoard()
+
+# createBoard()
 main()
